f4e678166d92e0ffde71dd7918b2a4c87f6a80bf
dillon-courts <dillon.courts@gmail.com>
2019-05-23 14:13:20 -0400

    Adding namespace as an option

--- generate_manifests.sh
+++ generate_manifests.sh
@@ -2,6 +2,7 @@ vault_url=$1
 role_id=$2
 secret_id=$3
 environment=$4
+namespace=$5
 
 client_token=$(curl -s -d "{\"role_id\":\"$role_id\", \"secret_id\":\"$secret_id\"}" -H "Content-Type: application/json" -X POST $vault_url/v1/auth/approle/login | jq -r '.auth.client_token')
 
@@ -13,8 +14,6 @@ management_username=$(echo $secrets | jq -r '.data.data.MANAGEMENT_USERNAME')
 management_password=$(echo $secrets | jq -r '.data.data.MANAGEMENT_PASSWORD')
 erlang_cookie=$(echo $secrets | jq -r '.data.data.ERLANG_COOKIE')
 
-echo $username
-
 mkdir -p manifests
 
-helm template --output-dir manifests --set rabbitmqUsername=$username,rabbitmqPassword=$password,managementUsername=$management_username,managementPassword=$management_password,rabbitmqErlangCookie=$erlang_cookie,fullnameOverride=crds-rabbit  helm-chart/
+helm template --output-dir manifests --set rabbitmqUsername=$username,rabbitmqPassword=$password,managementUsername=$management_username,managementPassword=$management_password,rabbitmqErlangCookie=$erlang_cookie,namespace=$namespace,fullnameOverride=crds-rabbit  helm-chart/
